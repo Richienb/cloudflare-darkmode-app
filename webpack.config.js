e367339b3214401ae2dee1bbecfd418609a53402
Richie Bendall <richiebendall@gmail.com>
2019-05-18 15:11:23 +1200

    Update code

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require("path")
+const webpack = require("webpack")
+
+module.exports = {
+    mode: "development",
+    devtool: "hidden-source-map",
+    entry: {
+        index: "./src/index.js",
+    },
+    output: {
+        filename: "[name].js",
+        sourceMapFilename: "[name].map",
+        path: path.resolve(__dirname, "build"),
+    },
+    module: {
+        rules: [{
+                test: /src\/.*\.js$/,
+                loader: "babel-loader",
+                exclude: /(node_modules)/,
+                options: {
+                    compact: false,
+                    presets: [
+                        [
+                            "env",
+                            {
+                                targets: {
+                                    browsers: ["last 2 versions", "ie >= 10"],
+                                },
+                            },
+                        ],
+                    ],
+                },
+            },
+
+            {
+                test: /workers\/.*\.js$/,
+                loader: "babel-loader",
+                exclude: /(node_modules)/,
+                options: {
+                    compact: false,
+                    presets: [
+                        [
+                            "env",
+                            {
+                                targets: {
+                                    node: "current",
+                                },
+                            },
+                        ],
+                    ],
+                },
+            },
+
+            {
+                test: /\.css$/,
+                use: [{
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[ext]",
+                        },
+                    },
+                    "extract-loader",
+                    "css-loader",
+                ],
+            },
+        ],
+    },
+}
